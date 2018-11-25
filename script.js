8a2af3878b32b614332da3201c9784de8a71ce1e
Samuel Robinson <pinkstarsr11@gmail.com>
2018-11-25 03:22:08 -0800

    Added mouse down diagnostic

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ const gridTapped = (x,y) => {
 
 canvas.addEventListener("mousedown",event => {
     if(!capturingTouch) {
+        console.log("mouse down");
         zoomInput.disabled = true;
         grid.hitDetectionX = event.clientX;
         grid.hitDetectionY = event.clientY;
